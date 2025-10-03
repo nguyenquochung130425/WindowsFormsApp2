b8eb1a3b958359a12672fcc164a28d87da9327a3
ADMIN <ADMIN@LAPTOP-CCDQE8TQ>
2025-10-03 15:06:14 +0700

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem heThongToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dinhDangToolStripMenuItem;
+        private System.Windows.Forms.ComboBox cbFont;
+        private System.Windows.Forms.ComboBox cbSize;
+        private System.Windows.Forms.Button btnBold;
+        private System.Windows.Forms.Button btnItalic;
+        private System.Windows.Forms.Button btnUnderline;
+        private System.Windows.Forms.RichTextBox rtbEditor;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.heThongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dinhDangToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbFont = new System.Windows.Forms.ComboBox();
+            this.cbSize = new System.Windows.Forms.ComboBox();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnUnderline = new System.Windows.Forms.Button();
+            this.rtbEditor = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            //
+            // menuStrip1
+            //
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.heThongToolStripMenuItem,
+            this.dinhDangToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            //
+            // heThongToolStripMenuItem
+            //
+            this.heThongToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thoatToolStripMenuItem});
+            this.heThongToolStripMenuItem.Name = "heThongToolStripMenuItem";
+            this.heThongToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.heThongToolStripMenuItem.Text = "Hệ thống";
+            //
+            // thoatToolStripMenuItem
+            //
+            this.thoatToolStripMenuItem.Name = "thoatToolStripMenuItem";
+            this.thoatToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.thoatToolStripMenuItem.Text = "Thoát";
+            this.thoatToolStripMenuItem.Click += new System.EventHandler(this.thoatToolStripMenuItem_Click);
+            //
+            // dinhDangToolStripMenuItem
+            //
+            this.dinhDangToolStripMenuItem.Name = "dinhDangToolStripMenuItem";
+            this.dinhDangToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.dinhDangToolStripMenuItem.Text = "Định dạng";
+            //
+            // cbFont
+            //
+            this.cbFont.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFont.FormattingEnabled = true;
+            this.cbFont.Location = new System.Drawing.Point(12, 40);
+            this.cbFont.Name = "cbFont";
+            this.cbFont.Size = new System.Drawing.Size(150, 21);
+            this.cbFont.TabIndex = 1;
+            this.cbFont.SelectedIndexChanged += new System.EventHandler(this.cbFont_SelectedIndexChanged);
+            //
+            // cbSize
+            //
+            this.cbSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSize.FormattingEnabled = true;
+            this.cbSize.Location = new System.Drawing.Point(180, 40);
+            this.cbSize.Name = "cbSize";
+            this.cbSize.Size = new System.Drawing.Size(60, 21);
+            this.cbSize.TabIndex = 2;
+            this.cbSize.SelectedIndexChanged += new System.EventHandler(this.cbSize_SelectedIndexChanged);
+            //
+            // btnBold
+            //
+            this.btnBold.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.btnBold.Location = new System.Drawing.Point(260, 40);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(30, 23);
+            this.btnBold.TabIndex = 3;
+            this.btnBold.Text = "B";
+            this.btnBold.UseVisualStyleBackColor = true;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            //
+            // btnItalic
+            //
+            this.btnItalic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic);
+            this.btnItalic.Location = new System.Drawing.Point(300, 40);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(30, 23);
+            this.btnItalic.TabIndex = 4;
+            this.btnItalic.Text = "I";
+            this.btnItalic.UseVisualStyleBackColor = true;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            //
+            // btnUnderline
+            //
+            this.btnUnderline.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline);
+            this.btnUnderline.Location = new System.Drawing.Point(340, 40);
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(30, 23);
+            this.btnUnderline.TabIndex = 5;
+            this.btnUnderline.Text = "U";
+            this.btnUnderline.UseVisualStyleBackColor = true;
+            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            //
+            // rtbEditor
+            //
+            this.rtbEditor.Location = new System.Drawing.Point(12, 70);
+            this.rtbEditor.Name = "rtbEditor";
+            this.rtbEditor.Size = new System.Drawing.Size(776, 368);
+            this.rtbEditor.TabIndex = 6;
+            this.rtbEditor.Text = "";
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbEditor);
+            this.Controls.Add(this.btnUnderline);
+            this.Controls.Add(this.btnItalic);
+            this.Controls.Add(this.btnBold);
+            this.Controls.Add(this.cbSize);
+            this.Controls.Add(this.cbFont);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Soạn thảo văn bản";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadFont();
+            LoadSize();
+        }
+
+        // Nạp danh sách font
+        private void LoadFont()
+        {
+            foreach (FontFamily font in FontFamily.Families)
+            {
+                cbFont.Items.Add(font.Name);
+            }
+            cbFont.SelectedItem = "Tahoma"; // mặc định
+        }
+
+        // Nạp danh sách cỡ chữ
+        private void LoadSize()
+        {
+            for (int i = 8; i <= 72; i += 2)
+            {
+                cbSize.Items.Add(i);
+            }
+            cbSize.SelectedItem = 14; // mặc định
+        }
+
+        // Đổi font
+        private void cbFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFontStyle();
+        }
+
+        // Đổi size
+        private void cbSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFontStyle();
+        }
+
+        // Hàm đổi định dạng
+        private void ChangeFontStyle()
+        {
+            if (cbFont.SelectedItem == null || cbSize.SelectedItem == null) return;
+
+            string fontName = cbFont.SelectedItem.ToString();
+            float fontSize = float.Parse(cbSize.SelectedItem.ToString());
+
+            FontStyle style = FontStyle.Regular;
+
+            if (btnBold.Font.Bold) style |= FontStyle.Bold;
+            if (btnItalic.Font.Italic) style |= FontStyle.Italic;
+            if (btnUnderline.Font.Underline) style |= FontStyle.Underline;
+
+            rtbEditor.SelectionFont = new Font(fontName, fontSize, style);
+        }
+
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+            btnBold.Font = new Font(btnBold.Font, btnBold.Font.Bold ? FontStyle.Regular : FontStyle.Bold);
+            ChangeFontStyle();
+        }
+
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+            btnItalic.Font = new Font(btnItalic.Font, btnItalic.Font.Italic ? FontStyle.Regular : FontStyle.Italic);
+            ChangeFontStyle();
+        }
+
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+            btnUnderline.Font = new Font(btnUnderline.Font, btnUnderline.Font.Underline ? FontStyle.Regular : FontStyle.Underline);
+            ChangeFontStyle();
+        }
+
+        // Menu Thoát
+        private void thoatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
